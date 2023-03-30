001ef5d88246b41826bce28ddb5ddd10e2248c34
RajanPatel098 <106442470+RajanPatel098@users.noreply.github.com>
2023-03-30 12:57:03 -0500

    Part one
    Fix logout
    Fix User Profile
    Fix User card -- add bio and headers
    Allow for user profile edit
    Fix homepage -only show last 100 msg from yourself and who you follow

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
+app.app_context().push()
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
@@ -113,8 +114,10 @@ def login():
 def logout():
     """Handle logout of user."""
 
-    # IMPLEMENT THIS
+    do_logout()
 
+    flash("YOU JAVE LOGGED OUT.", 'success')
+    return redirect("/login")
 
 ##############################################################################
 # General user routes:
@@ -211,8 +214,25 @@ def stop_following(follow_id):
 def profile():
     """Update profile for current user."""
 
-    # IMPLEMENT THIS
+    if not g.user:
+        flash("Access Unauth", "danger")
+        return redirect("/")
+    user = g.user
+    form = UserEditForm(obj=user)
 
+    if form.validate_on_submit():
+        if User.authenticate(user.username, form.password.data):
+            user.username = form.username.data
+            user.email = form.email.data
+            user.image_url = form.image_url.data or "/static/images/default-pic.png"
+            user.header_image_url = form.header_image_url.data or "/static/images/warbler-hero.jpg"
+            user.bio = form.bio.data
+
+            db.session.commit()
+            return redirect(f"/users/{user.id}")
+        
+        flash("Wrong password, try again", "danger")
+    return render_template('users/edit.html', form=form, user_id=user.id)
 
 @app.route('/users/delete', methods=["POST"])
 def delete_user():
@@ -292,8 +312,11 @@ def homepage():
     """
 
     if g.user:
+        following_ids = [f.id for f in g.user.following] + [g.user.id]
+
         messages = (Message
                     .query
+                    .filter(Message.user_id.in_(following_ids))
                     .order_by(Message.timestamp.desc())
                     .limit(100)
                     .all())
--- forms.py
+++ forms.py
@@ -17,6 +17,15 @@ class UserAddForm(FlaskForm):
     password = PasswordField('Password', validators=[Length(min=6)])
     image_url = StringField('(Optional) Image URL')
 
+class UserEditForm(FlaskForm):
+    """FORM FOR EDITING EXISTING USERS"""
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    image_url = StringField('(Optional) Image URL')
+    header_image_url = StringField('(Optional) Header Image URL')
+    bio = TextAreaField('(Optional) Tell everyone about yourself')
+    location = TextAreaField('(Optional) Where are you from?')
+    password = PasswordField('Password', validators=[Length(min=6)])
 
 class LoginForm(FlaskForm):
     """Login form."""
